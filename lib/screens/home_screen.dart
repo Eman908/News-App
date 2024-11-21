6e88c4f3c89b860e26a5e482fbe5da586fccb78e
EmanT <tharwateman38@gmail.com>
2024-11-21 08:21:32 +0200

    add appbar to CustomScrollView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,26 +8,32 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-        title: RichText(
-            text: TextSpan(
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                children: <TextSpan>[
-              const TextSpan(
-                  text: 'News', style: TextStyle(color: Colors.black)),
-              TextSpan(
-                  text: 'Cloud', style: TextStyle(color: Colors.amber[900])),
-            ])),
-      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: CustomScrollView(
           physics: const BouncingScrollPhysics(),
           slivers: [
+            SliverAppBar(
+              floating: true, // AppBar reappears when you scroll up.
+              pinned: false, // AppBar doesn't stay fixed at the top.
+              snap: true, // Ensures the AppBar snaps into place when scrolling.
+              elevation: 0,
+              centerTitle: true,
+              title: RichText(
+                text: TextSpan(
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 24),
+                  children: <TextSpan>[
+                    const TextSpan(
+                        text: 'News', style: TextStyle(color: Colors.black)),
+                    TextSpan(
+                        text: 'Cloud',
+                        style: TextStyle(
+                            color: Colors.amber[900] ?? Colors.amber)),
+                  ],
+                ),
+              ),
+            ),
             SliverToBoxAdapter(child: CategoryList()),
             const SliverToBoxAdapter(
               child: SizedBox(
